c053be970620d3c1156c6a09a256d7311d60f983
Artem <artemmakarevich1997@gmail.com>
2020-04-01 13:47:14 +0300

    Start of proj

---
+++ 5.3.2OkHttpDemoApp/src/main/java/com/example/server_5_3_2/Server532Application.java
@@ -0,0 +1,13 @@
+package com.example.server_5_3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server532Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server532Application.class, args);
+    }
+
+}
---
+++ 5.3.2OkHttpDemoApp/src/test/java/com/example/server_5_3_2/Server532ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_5_3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server532ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
